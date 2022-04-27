ae1125119c109c257e9328ec3ca2afb2b46eb0c5
Marcel Rehkemper <marcel.rehkemper@cargosupport.de>
2022-04-27 16:42:16 +0200

    upgraded to .net 6, added typesafety

--- src/Unit-2/DoThis/Actors/ChartingActor.cs
+++ src/Unit-2/DoThis/Actors/ChartingActor.cs
@@ -1,74 +1,62 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿
 using System.Windows.Forms.DataVisualization.Charting;
-using Akka.Actor;
 
-namespace ChartApp.Actors
+using CargoSupport.Akka.Typed;
+
+namespace ChartApp.Actors;
+public record ChartingMessage : ActorMessage { }
+public class ChartingActor : Actor<ChartingActor, ChartingMessage>
 {
-    public class ChartingActor : UntypedActor
+    public class Messages
     {
-        #region Messages
-
-        public class InitializeChart
-        {
-            public InitializeChart(Dictionary<string, Series> initialSeries)
-            {
-                InitialSeries = initialSeries;
-            }
-
-            public Dictionary<string, Series> InitialSeries { get; private set; }
-        }
-
-        #endregion
-
-        private readonly Chart _chart;
-        private Dictionary<string, Series> _seriesIndex;
-
-        public ChartingActor(Chart chart) : this(chart, new Dictionary<string, Series>())
-        {
-        }
-
-        public ChartingActor(Chart chart, Dictionary<string, Series> seriesIndex)
-        {
-            _chart = chart;
-            _seriesIndex = seriesIndex;
-        }
-
-        protected override void OnReceive(object message)
-        {
-            if (message is InitializeChart)
-            {
-                var ic = message as InitializeChart;
-                HandleInitialize(ic);
-            }
-        }
-
-        #region Individual Message Type Handlers
-
-        private void HandleInitialize(InitializeChart ic)
-        {
-            if (ic.InitialSeries != null)
-            {
-                //swap the two series out
-                _seriesIndex = ic.InitialSeries;
-            }
-
-            //delete any existing series
-            _chart.Series.Clear();
-
-            //attempt to render the initial chart
-            if (_seriesIndex.Any())
-            {
-                foreach (var series in _seriesIndex)
-                {
-                    //force both the chart and the internal index to use the same names
-                    series.Value.Name = series.Key;
-                    _chart.Series.Add(series.Value);
-                }
-            }
-        }
-
-        #endregion
+        public record InitializeChart(Dictionary<string, Series>? InitialSeries) : ChartingMessage;
     }
+
+    private readonly Chart _chart;
+    private Dictionary<string, Series> _seriesIndex;
+
+    public ChartingActor(Chart chart) : this(chart, new())
+    {
+    }
+
+    public ChartingActor(Chart chart, Dictionary<string, Series> seriesIndex)
+    {
+        _chart = chart;
+        _seriesIndex = seriesIndex;
+    }
+
+    protected override void OnReceive(ChartingMessage message)
+    {
+        switch (message)
+        {
+            case Messages.InitializeChart initChart:
+                HandleInitialize(initChart);
+                break;
+        }
+    }
+
+    #region Individual Message Type Handlers
+
+    private void HandleInitialize(Messages.InitializeChart ic)
+    {
+        if (ic.InitialSeries is not null)
+            //swap the two series out
+            _seriesIndex = ic.InitialSeries;
+
+        //delete any existing series
+        _chart.Series.Clear();
+
+        //attempt to render the initial chart
+        if (!_seriesIndex.Any())
+            return;
+
+        foreach (var (key, value) in _seriesIndex)
+        {
+            //force both the chart and the internal index to use the same names
+            value.Name = key;
+            _chart.Series.Add(value);
+        }
+    }
+
+    #endregion
 }
--- src/Unit-2/DoThis/Main.cs
+++ src/Unit-2/DoThis/Main.cs
@@ -1,9 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Windows.Forms;
-using System.Windows.Forms.DataVisualization.Charting;
+﻿using System.Windows.Forms.DataVisualization.Charting;
+
 using Akka.Actor;
 using Akka.Util.Internal;
+
 using ChartApp.Actors;
 
 namespace ChartApp
@@ -25,7 +24,7 @@ namespace ChartApp
         {
             _chartActor = Program.ChartActors.ActorOf(Props.Create(() => new ChartingActor(sysChart)), "charting");
             var series = ChartDataHelper.RandomSeries("FakeSeries" + _seriesCounter.GetAndIncrement());
-            _chartActor.Tell(new ChartingActor.InitializeChart(new Dictionary<string, Series>()
+            _chartActor.Tell(new ChartingActor.Messages.InitializeChart(new Dictionary<string, Series>()
             {
                 {series.Name, series}
             }));
