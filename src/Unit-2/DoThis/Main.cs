a49d0c4ab4202edeaefa142b4d04fc5aff1fee6f
Marcel Rehkemper <marcel.rehkemper@cargosupport.de>
2022-04-28 12:10:02 +0200

    added missing typesafety

--- src/Unit-2/DoThis/Main.cs
+++ src/Unit-2/DoThis/Main.cs
@@ -1,7 +1,8 @@
-﻿using System.Windows.Forms.DataVisualization.Charting;
-
+﻿
 using Akka.Actor;
 using Akka.Util.Internal;
+
+using CargoSupport.Akka.Typed;
 
 using ChartApp.Actors;
 
@@ -9,8 +10,8 @@ namespace ChartApp
 {
     public partial class Main : Form
     {
-        private IActorRef _chartActor;
-        private readonly AtomicCounter _seriesCounter = new AtomicCounter(1);
+        private IActorRef<ChartingMessage>? _chartActor;
+        private readonly AtomicCounter _seriesCounter = new(1);
 
         public Main()
         {
@@ -22,18 +23,18 @@ namespace ChartApp
 
         private void Main_Load(object sender, EventArgs e)
         {
-            _chartActor = Program.ChartActors.ActorOf(Props.Create(() => new ChartingActor(sysChart)), "charting");
+            _chartActor = Program.ChartActors.ActorOf<ChartingActor, ChartingMessage>(() => new(sysChart), "charting");
             var series = ChartDataHelper.RandomSeries("FakeSeries" + _seriesCounter.GetAndIncrement());
-            _chartActor.Tell(new ChartingActor.Messages.InitializeChart(new Dictionary<string, Series>()
+            _chartActor.Tell(new ChartingActor.Messages.InitializeChart(new()
             {
-                {series.Name, series}
+                { series.Name, series }
             }));
         }
 
         private void Main_FormClosing(object sender, FormClosingEventArgs e)
         {
             //shut down the charting actor
-            _chartActor.Tell(PoisonPill.Instance);
+            _chartActor!.SysTell(PoisonPill.Instance);
 
             //shut down the ActorSystem
             Program.ChartActors.Terminate();
